34ac24d452f193742b4a64e982a122240b3e67ab
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2023-08-16 11:59:35 -0400

    state requirement & modular config vars

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 PORT = 3333
-thisUrl = "https://87c3-68-234-232-23.ngrok-free.app"
+thisUrl = "YOUR_ROOT_URL_HERE"
+verificationToken = "YOUR_VERIFICATION_TOKEN_HERE"
 
 # Note: Setting CORS to allow chat.openapi.com is required for ChatGPT to access your plugin
 CORS(app, origins=[thisUrl, "https://chat.openai.com"])
@@ -20,29 +21,46 @@ _TODO = []
 
 @app.route('/.well-known/ai-plugin.json')
 def serve_manifest():
-    return send_from_directory(os.path.dirname(__file__), 'ai-plugin.json')
+    with open(os.path.join(os.path.dirname(__file__), 'ai-plugin.json'), 'r') as f:
+        content = f.read()
+    
+    # Replace PLUGIN_HOSTNAME with thisUrl
+    modified_content = content.replace('PLUGIN_HOSTNAME', thisUrl).replace('VERIFICATION_TOKEN', verificationToken)
+
+    
+    return Response(modified_content, content_type='application/json')
+
 
 
 @app.route('/openapi.yaml')
 def serve_openapi_yaml():
     with open(os.path.join(os.path.dirname(__file__), 'openapi.yaml'), 'r') as f:
         yaml_data = f.read()
-    yaml_data = yaml.load(yaml_data, Loader=yaml.FullLoader)
-    return jsonify(yaml_data)
+
+    # Replace PLUGIN_HOSTNAME with thisUrl
+    modified_yaml_data = yaml_data.replace('PLUGIN_HOSTNAME', thisUrl)
+
+    # Convert the YAML string to a Python dictionary and then jsonify it
+    yaml_dict = yaml.load(modified_yaml_data, Loader=yaml.FullLoader)
+    return jsonify(yaml_dict)
+
 
 
 @app.route('/openapi.json')
 def serve_openapi_json():
-    return send_from_directory(os.path.dirname(__file__), 'openapi.json')
+    with open(os.path.join(os.path.dirname(__file__), 'openapi.json'), 'r') as f:
+        json_data = f.read()
+
+    # Replace PLUGIN_HOSTNAME with thisUrl
+    modified_json_data = json_data.replace('PLUGIN_HOSTNAME', thisUrl)
+
+    return Response(modified_json_data, content_type='application/json')
+
 
 
 @app.route('/todos', methods=['GET', 'POST'])
 def wrapper():
     global _TODO
-
-    headers = {
-        'Content-Type': 'application/json',
-    }
 
     if request.method == 'GET':
         # Get the list of todos
@@ -75,12 +93,8 @@ def oauth():
         v = v.replace("%2F", "/").replace("%3A", ":")
         kvps[k] = v
     print("OAuth key value pairs from the ChatGPT Request: ", kvps)
-    url = kvps["redirect_uri"] + f"?code={OPENAI_CODE}"
+    url = kvps["redirect_uri"] + f"?code={OPENAI_CODE}&state={kvps['state']}"
     print("URL: ", url)
-    # translate the following quart code to flask
-    # return quart.Response(
-    #     f'<a href="{url}">Click to authorize</a>'
-    # )
     return f'<a href="{url}">Click to authorize</a>'
 
 # Sample names
